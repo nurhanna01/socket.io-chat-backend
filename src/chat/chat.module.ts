d4caf368d8931fbebed519c49797c02217b3b6c0
Nur Hanna <nurhanna01@users.noreply.github.com>
2025-03-14 22:10:27 +0700

    update event join app & list message

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,9 +4,10 @@ import { Message } from 'src/entities/message.entity';
 import { Users } from 'src/entities/users.entity';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
+import { Rooms } from 'src/entities/rooms.entity';
 console.log('chat module');
 @Module({
-  imports: [TypeOrmModule.forFeature([Message, Users])],
+  imports: [TypeOrmModule.forFeature([Message, Users, Rooms])],
   providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -11,6 +11,3 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   entities: [process.cwd() + '/dist/**/*.entity{.ts,.js}'],
   synchronize: false,
 };
-console.log('All env vars:', process.env);
-console.log('Database user:', process.env.DATABASE_USER);
-console.log('Env file path:', process.cwd());
